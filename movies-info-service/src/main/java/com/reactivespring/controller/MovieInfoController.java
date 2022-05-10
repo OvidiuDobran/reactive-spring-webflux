9c7b117dc4b34eae623fe0410bfb65299ffa9d17
ODOBRAN <ovidiu.dobran@computacenter.com>
2022-05-10 14:47:10 +0300

    Added unit tests, bean validations, and global error handler

--- movies-info-service/src/main/java/com/reactivespring/controller/MovieInfoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/MovieInfoController.java
@@ -3,9 +3,12 @@ package com.reactivespring.controller;
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.service.MovieInfoService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/v1")
@@ -20,18 +23,19 @@ public class MovieInfoController {
 
     @PostMapping("/movieinfo")
     @ResponseStatus(HttpStatus.CREATED)
-    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo) {
+    public Mono<MovieInfo> addMovieInfo(@RequestBody @Valid MovieInfo movieInfo) {
         return movieInfoService.addMovieInfo(movieInfo);
     }
 
     @GetMapping("/movieinfos")
-    public Flux<MovieInfo> getAllMovieInfos(){
+    public Flux<MovieInfo> getAllMovieInfos() {
         return movieInfoService.getAllMovieInfos();
     }
 
-    @GetMapping("/movieinfos/{id}")
-    public Mono<MovieInfo> getMovieInfoById(@PathVariable String id){
-        return movieInfoService.getMovieInfoById(id);
+    @GetMapping("/movieinfo/{id}")
+    public Mono<ResponseEntity<MovieInfo>> getMovieInfoById(@PathVariable String id) {
+        return movieInfoService.getMovieInfoById(id).map(ResponseEntity.ok()::body)
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
 
 }
---
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/MoviesInfoControllerUnitTest.java
@@ -0,0 +1,63 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.service.MovieInfoService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+
+@WebFluxTest(controllers = MovieInfoController.class)
+@AutoConfigureWebTestClient
+public class MoviesInfoControllerUnitTest {
+
+    static final String MOVIES_INFOS_URL = "/v1/movieinfos";
+
+    static final String MOVIE_INFO_URL = "/v1/movieinfo";
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @MockBean
+    private MovieInfoService movieInfoServiceMock;
+
+    @Test
+    void getAllMoviesInfo() {
+
+        var movies = List.of(new MovieInfo(null, "Batman Begins", 2005, List.of("Christian Bale", "Michael Cane"),
+                        LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight", 2008, List.of("Christian Bale", "Heath Ledger"),
+                        LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises", 2012, List.of("Christian Bale", "Tom Hardy"),
+                        LocalDate.parse("2012-07-20")));
+
+        when(movieInfoServiceMock.getAllMovieInfos()).thenReturn(Flux.fromIterable(movies));
+
+        webTestClient.get().uri(MOVIES_INFOS_URL).exchange().expectStatus().is2xxSuccessful()
+                .expectBodyList(MovieInfo.class).hasSize(3);
+    }
+
+    @Test
+    void getMovieInfoById() {
+        var expectedMovieInfo = new MovieInfo("abc", "Dark Knight Rises", 2012, List.of("Christian Bale", "Tom Hardy"),
+                LocalDate.parse("2012-07-20"));
+
+        when(movieInfoServiceMock.getMovieInfoById(isA(String.class))).thenReturn(Mono.just(expectedMovieInfo));
+
+        webTestClient.get().uri(MOVIE_INFO_URL + "/{id}", "abc").exchange().expectStatus().is2xxSuccessful()
+                .expectBody(MovieInfo.class).consumeWith(res -> assertNotNull(res.getResponseBody()));
+    }
+
+
+}
