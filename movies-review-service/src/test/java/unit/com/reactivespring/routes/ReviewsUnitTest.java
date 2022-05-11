81aa5c184a4d340c1d30376474789fe1f90d79d8
ODOBRAN <ovidiu.dobran@computacenter.com>
2022-05-11 15:48:29 +0300

    Unit test for add review

--- movies-review-service/src/test/java/unit/com/reactivespring/routes/ReviewsUnitTest.java
+++ movies-review-service/src/test/java/unit/com/reactivespring/routes/ReviewsUnitTest.java
@@ -1,4 +1,42 @@
 package com.reactivespring.routes;
 
+import com.reactivespring.domain.Review;
+import com.reactivespring.handler.ReviewHandler;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import com.reactivespring.router.ReviewRouter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static reactor.core.publisher.Mono.when;
+
+@WebFluxTest
+@ContextConfiguration(classes = {ReviewRouter.class, ReviewHandler.class})
+@AutoConfigureWebTestClient
 public class ReviewsUnitTest {
+
+    static final String REVIEWS_URL = "/v1/reviews";
+
+    @MockBean
+    private ReviewReactiveRepository reviewReactiveRepository;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void addReview() {
+        var review = new Review("id_1", 2L, "Excellent Movie", 8.0);
+
+        when(reviewReactiveRepository.save(isA(Review.class))).thenReturn(Mono.just(review));
+
+        webTestClient.post().uri(REVIEWS_URL).bodyValue(review).exchange().expectStatus().isCreated()
+                     .expectBody(Review.class).consumeWith(Assertions::assertNotNull);
+    }
 }
