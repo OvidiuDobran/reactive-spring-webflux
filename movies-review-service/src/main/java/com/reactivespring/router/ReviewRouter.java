82560865f92db40814a6dfd290adc766202156ef
ODOBRAN <ovidiu.dobran@computacenter.com>
2022-05-11 15:38:37 +0300

    Implemented reviews service using functional endpoints

---
+++ movies-review-service/src/main/java/com/reactivespring/router/ReviewRouter.java
@@ -0,0 +1,24 @@
+package com.reactivespring.router;
+
+import com.reactivespring.handler.ReviewHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class ReviewRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> reviewsRouter(ReviewHandler reviewHandler) {
+        return route().nest(
+                              path(""),
+                              builder -> builder.POST("", reviewHandler::addReview).GET("/v1/reviews",
+                                                                                        reviewHandler::getReviews)
+                                                .DELETE("/{id}", request -> reviewHandler.deleteReview(request)))
+                      .GET("/v1/helloworld", (request -> ServerResponse.ok().bodyValue("Hello World!"))).build();
+    }
+}
